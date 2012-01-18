455aaee5d1c5b5cd041d4d17628b99a1db9a3672
Remo Brunschwiler <remo@terrifically.org>
2012-01-18 16:59:47 +0100

    License

--- Entity/Module.php
+++ Entity/Module.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the Terrific Composer package.
+ *
+ * (c) Remo Brunschwiler <remo@terrifically.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Terrific\ComposerBundle\Entity;
 
 use Symfony\Component\Validator\Constraints as Assert;
--- Template/Module/TerrificModuleDefaultBundle.php
+++ Template/Module/TerrificModuleDefaultBundle.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the Terrific Composer package.
+ *
+ * (c) Remo Brunschwiler <remo@terrifically.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Terrific\Module\DefaultBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
